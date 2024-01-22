0e98aad6f48d4dba0822c331a83dc19df1dcd2b3
harumakiohara <ktq2390319@stu.o-hara.ac.jp>
2024-01-22 12:16:32 +0900

    java

---
+++ ensyuu12/Keisyou.java
@@ -0,0 +1,31 @@
+package ensyuu12;
+
+import java.util.Scanner;
+import java.lang.Math;
+
+class Calculator {
+    Scanner stdIn = new Scanner(System.in);
+
+    void Hikisuu(){
+        int x = stdIn.nextInt();
+        int y = stdIn.nextInt();
+        return;
+    }
+
+    void ClacSum(int x, int y){
+        int sum = x + y;
+        System.out.println("Sum " + x + " and " + y + " = " + ave);
+    }
+    
+    void ClacAve(int x, int y){
+        int ave = (x + y) / 2;
+        System.out.println("Average " + x + " and " + y + " = " + ave);
+    }
+}
+
+
+class MoreClac extends Calculator{
+    void clacPow(int x, int y){
+        int pow = x * 
+    }
+}
