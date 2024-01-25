e7dce388e7a28666e86d7d3ba7e08d96137736a3
harumakiohara <ktq2390319@stu.o-hara.ac.jp>
2024-01-25 14:02:42 +0900

    java

--- ensyuu13/Main.java
+++ ensyuu13/Main.java
@@ -1,8 +1,9 @@
 package ensyuu13;
 
+//抽象クラスPerson
 abstract class Person {
-    String name = "";
-    String job = "";
+    public String name = "";
+    public String job = "";
     
     //抽象メソッド
     abstract void introduce();
@@ -12,6 +13,54 @@ abstract class Person {
         this.name = name;
         this.job = job;
     }
-
     
+}
+
+//Personを継承したTeacherクラス
+class Teacher extends Person{
+    private String subject = "";
+
+    public Teacher(String name, String job, String subject) {
+        super(name, job);
+        this.subject = subject;
+    }
+    
+    @Override public void introduce(){
+        System.out.println("氏名:" + this.name);
+        System.out.println("職業:" + this.job);
+        System.out.println("担当科目:" + this.subject);
+    }
+    
+
+}
+
+//Personを継承したCookクラス
+class Cook extends Person{
+    private String specialties = "";
+
+    public Cook(String name, String job, String specialties) {
+        super(name, job);
+        this.specialties = specialties;
+    }
+    
+    
+
+    @Override public void introduce(){
+        System.out.println("氏名:" + this.name);
+        System.out.println("職業:" + this.job);
+        System.out.println("得意料理:" + this.specialties);
+    }
+    
+
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Teacher teacher = new Teacher("竹井一馬", "教員", "情報処理");
+        Cook cook = new Cook("大原太郎", "シェフ", "オムライス");
+
+        teacher.introduce();
+        System.out.println();
+        cook.introduce();
+    }
 }
