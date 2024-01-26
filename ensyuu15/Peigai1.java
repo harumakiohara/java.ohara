8d7e49e7bfb631eb7abd152e8c29ab385646687c
harumakiohara <ktq2390319@stu.o-hara.ac.jp>
2024-01-26 14:33:55 +0900

    java

--- ensyuu15/Peigai1.java
+++ ensyuu15/Peigai1.java
@@ -5,17 +5,17 @@ import java.util.Scanner;
 public class Peigai1 {
     public static void main(String[] args) {
          Scanner stdIn =new Scanner(System.in);
+        try{
          System.out.print("整数1 = ");
          int s1 = stdIn.nextInt();
          System.out.print("整数2 = ");
          int s2  = stdIn.nextInt();
-         if (s2 == 0){
-             System.out.println("0による割り算です!!");
-         }
-         else{
-            double s = s1 / s2; 
-            System.out.println(s1 + " / " + s2 + " = " + s);
-         }
+         double s = s1 / s2; 
+         System.out.println(s1 + " / " + s2 + " = " + s);
+        }catch(ArithmeticException e){
+            System.out.println("0による割り算です!!");
+        }
+         
 
     }
 }
