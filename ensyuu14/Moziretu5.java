06ca9da1a2c6e4c143bb0477f96bc418b9d0b956
harumakiohara <ktq2390319@stu.o-hara.ac.jp>
2024-01-29 12:16:21 +0900

    java

---
+++ ensyuu14/Moziretu5.java
@@ -0,0 +1,10 @@
+package ensyuu14;
+
+public class Moziretu5 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("東京都千代田区");
+        sb.append("神田神保町");
+        System.out.println(sb);
+    }
+}
